7c7a1664c64ca5ca83845ddfe0aa831941e1aa97
Lucifer Morningstar <shankhabrato580@gmail.com>
2022-04-06 19:48:16 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -63,8 +63,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Report Group", "https://t.me/+PRRzqHd31XY3ZWZk")
+    buttons.buildbutton("Repo", "https://github.com/Lucifer-Royal-Gaming/mirror-leech-telegram-heroku-bot")
+    buttons.buildbutton("Report Group", "https://t.me/luciferultrongaming")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
